b2e65f7d5b51996ee1d2b530ef40e5d76a1ee6cd
arsee <arsee@sina.com>
2018-04-26 00:43:18 +0800

    update rtp_client

--- examples/rtp_client/rtp_client.cpp
+++ examples/rtp_client/rtp_client.cpp
@@ -22,7 +22,9 @@ using namespace std;
 #include "av_rtp_source.h"
 #include "av_decode_filter.h"
 #include "av_exception.h"
-#include "../av_displayer.h"
+//#include "../av_displayer.h"
+#include "h264_rtp_packer.h"
+#include <av_log.h>
 
 int main(int argc, char* argv[])
 {
@@ -32,12 +34,13 @@ int main(int argc, char* argv[])
 #endif
 
 	av_init();
+	av_set_logger(stdout_log);
 
 	try {
-		AVDisplayer dis;
-        dis.open();
-		AvDecodeFilter* df = AvDecodeFilter::create(CodecID::H264, &dis);
-		AvRtpSource* rtp = AvRtpSource::create(df, 8000);
+//		AVDisplayer dis;
+//	        dis.open();
+		AvDecodeFilter* df = AvDecodeFilter::create(CodecID::H264, nullptr);
+		AvRtpSource<H264RtpPacker>* rtp = AvRtpSource<H264RtpPacker>::create(df, 8002, H264RtpPacker(25));
 		
         while(true)
         {
